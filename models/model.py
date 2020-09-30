1e52533286b5035483131eb79cc98345c5a2defa
DanteTemplar <69670642+DanteTemplar@users.noreply.github.com>
2020-09-30 22:26:45 +0700

    tesseract commit

--- models/model.py
+++ models/model.py
@@ -1,1 +1,13 @@
-#weights = '../weights'
+try:
+    from PIL import Image #OpenCV быстрей
+except ImportError:
+    import Image
+import pytesseract
+
+# If you don't have tesseract executable in your PATH, include the following:
+pytesseract.pytesseract.tesseract_cmd = r'F:\Codes\Projects\ML\KRUZHOK.PRO\application-for-KRUZHOK.PRO\models\tesseract\tesseract'
+# Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
+
+pdf = pytesseract.image_to_pdf_or_hocr('test.jpg', extension='pdf', lang='rus')
+with open('test.pdf', 'w+b') as f:
+    f.write(pdf)
