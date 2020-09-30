8bec7f7384d89c92015ab6c29375a67eccb37711
DanteTemplar <69670642+DanteTemplar@users.noreply.github.com>
2020-09-30 23:00:07 +0700

    delete .idea

--- models/model.py
+++ models/model.py
@@ -5,7 +5,7 @@ except ImportError:
 import pytesseract
 
 # If you don't have tesseract executable in your PATH, include the following:
-pytesseract.pytesseract.tesseract_cmd = r'F:\Codes\Projects\ML\KRUZHOK.PRO\application-for-KRUZHOK.PRO\models\tesseract\tesseract'
+pytesseract.pytesseract.tesseract_cmd = r'F:\Codes\Projects\ML\KRUZHOK.PRO\tesseract\tesseract'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
 
 pdf = pytesseract.image_to_pdf_or_hocr('test.jpg', extension='pdf', lang='rus')
